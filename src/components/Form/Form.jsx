b86e02c384d0b034804bb5479a9b38379a73cdff
Hugomndez <hello@hugomendez.dev>
2022-06-27 09:38:41 -0500

    Add Form + ChartsLoader components.

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,83 @@
+//import { useContext } from 'react';
+//import { FetchContext } from '../../contexts';
+
+import styles from './Form.module.css';
+
+const Form = () => {
+  //const { setQuery, setToken } = useContext(FetchContext);
+
+  const onKeyEnter = async event => {
+    if (event.key === 'Enter') {
+      await handleSubmit(event);
+    }
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    const tokenToFetch = event.currentTarget.token.value;
+    const seriesToFetch = event.currentTarget.series.value;
+
+    if (!tokenToFetch && !seriesToFetch) {
+      return null;
+    }
+
+    if (tokenToFetch.length !== 64) {
+      return null;
+      //Handle Wrong token.
+    }
+
+    setQuery(seriesToFetch);
+
+    setToken(tokenToFetch);
+
+    event.currentTarget.series.value = '';
+
+    // eslint-disable-next-line no-unused-vars
+    //const Chart = (await import('chart.js/auto')).default;
+  };
+
+  return (
+    <form id={styles.apiForm} onSubmit={handleSubmit} onKeyPress={onKeyEnter}>
+      <fieldset>
+        <label htmlFor='token'>Token</label>
+        <input
+          itemType='text'
+          id='token'
+          name='token'
+          placeholder='Add Banxico Token'
+          required
+          autoFocus
+        ></input>
+        <p>
+          You need a Banxico token. You can find it on: <br></br>
+          <a
+            href='https://www.banxico.org.mx/SieAPIRest/service/v1/token'
+            target='_blank'
+            rel='noreferrer'
+          >
+            https://www.banxico.org.mx/SieAPIRest/service/v1/token
+          </a>
+        </p>
+      </fieldset>
+      <fieldset>
+        <label htmlFor='series'>Series</label>
+
+        <input
+          itemType='text'
+          id='series'
+          name='series'
+          placeholder='Add Banxico Series'
+          required
+        ></input>
+
+        <p>Example: SF61745,SP68257</p>
+      </fieldset>
+      <fieldset>
+        <button type='submit'>Fetch</button>
+      </fieldset>
+    </form>
+  );
+};
+
+export default Form;
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,1 +1,3 @@
+export { default as ChartsLoader } from './ChartsLoader';
+export { default as Form } from './Form';
 export { default as Header } from './Header';
