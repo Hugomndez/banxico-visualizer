402812e99fecbfbc82dddb4a320c18929b2058e0
Hugomndez <hello@hugomendez.dev>
2022-06-27 20:42:07 -0500

    Fix css modules camelCase.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: config => {
+    // camel-case style names from css modules
+    config.module.rules
+      .find(({ oneOf }) => !!oneOf)
+      .oneOf.filter(({ use }) => JSON.stringify(use)?.includes('css-loader'))
+      .reduce((acc, { use }) => acc.concat(use), [])
+      .forEach(({ options }) => {
+        if (options.modules) {
+          options.modules.exportLocalsConvention = 'camelCase';
+        }
+      });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
